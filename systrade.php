7d58859d6b5fc1b46c850a9bfa0da17e29a8b237
Jirayouth <Jirayouth@users.noreply.github.com>
2017-01-21 22:46:51 +0700

    Update systrade.php

--- systrade.php
+++ systrade.php
@@ -15,7 +15,15 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Get user
-			$user = $event['source']['text'];
+			$user = $event['source']['type'];
+			if($user=='user'){
+				$uid = $event['source']['userId'];
+			}else if($user=='group'){
+				$uid = $event['source']['groupId'];
+			}else {
+				$uid = $event['source']['roomId'];
+			}
+			
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
@@ -39,10 +47,31 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 			echo $result . "\r\n";
 			
+			// PUSH Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$send_message = $user." ".$uid." : ".$text;
+			$messages_push = [
+				'type' => 'text',
+				'text' => $send_message
+			];
+			$data = [
+				'to' => "C7b6f92fef09462cb0942c24b33820ceb",
+				'messages' => [$messages_push],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+			
 			$myfile = fopen("nm_list.txt", "w") or die("Unable to open file!");
 			$txt = $user+" : "+$replyToken+"\n";
-			fwrite($myfile, $txt);
-			$txt = "Minnie Mouse\n";
 			fwrite($myfile, $txt);
 			fclose($myfile);
 		}
